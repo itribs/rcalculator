fbe59b98568eb90ba773efc1c4607ca196d21d03
黄泽新 <234126357@qq.com>
2020-10-29 17:50:24 +0800

    keep preload

--- vue.config.js
+++ vue.config.js
@@ -7,14 +7,15 @@ module.exports = {
     productionSourceMap: false,
     filenameHashing: false,
     configureWebpack: {
-        entry: {
-            'preload': './src/preload.js'
-        },
+        // entry: {
+        //     'preload': './src/preload.js'
+        // },
         plugins: [
             new FileManagerPlugin({
                 onEnd: {
                     move: [
-                        { source: path.resolve("./dist/static/js/preload.js"), destination: path.resolve("./dist/preload.js") }
+                        { source: path.resolve("./src/interpreter"), destination: path.resolve("./dist/interpreter") },
+                        { source: path.resolve("./src/preload.js"), destination: path.resolve("./dist/preload.js") }
                     ]
                 }
             })
